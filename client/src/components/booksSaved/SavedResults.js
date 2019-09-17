14f6fbe1d9acae6548fd79d8b411d14cda9407c3
Josh Latour <jlatour88@gmail.com>
2019-09-16 21:34:29 -0500

    changed to function from class

--- client/src/components/booksSaved/SavedResults.js
+++ client/src/components/booksSaved/SavedResults.js
@@ -1,33 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class BookResults extends Component {
-   render() {
-      return (
-         <div className='card border-secondary' style={{ width: '18rem' }}>
-            <img
-               className='card-img-top'
-               src='https://via.placeholder.com/150'
-               alt='Book Img'
-            ></img>
-            <div className='card-body'>
-               <h5 className='card-title'>Book Title</h5>
-               <sub className='card-authors'>Written by:</sub>
-               <p className='card-text' style={{ paddingTop: '1rem' }}>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-               </p>
-               <div className='row justify-content-between'>
-                  <a href='/' className='btn btn-primary col-5'>
-                     View
-                  </a>
-                  <a href='/' className='btn btn-primary col-5'>
-                     Save
-                  </a>
-               </div>
+const SavedResults = (props) => {
+   return (
+      <div className='card border-secondary' style={{ width: '18rem' }}>
+         <img
+            className='card-img-top'
+            src='https://via.placeholder.com/150'
+            alt='Book Img'
+         ></img>
+         <div className='card-body'>
+            <h5 className='card-title'>Book Title</h5>
+            <sub className='card-authors'>Written by:</sub>
+            <p className='card-text' style={{ paddingTop: '1rem' }}>
+               Some quick example text to build on the card title and make up
+               the bulk of the card's content.
+            </p>
+            <div className='row justify-content-between'>
+               <a href='/' className='btn btn-primary col-5'>
+                  View
+               </a>
+               <a href='/' className='btn btn-primary col-5'>
+                  Save
+               </a>
             </div>
          </div>
-      );
-   }
+      </div>
+   );
 }
 
-export default BookResults;
+export default SavedResults;
