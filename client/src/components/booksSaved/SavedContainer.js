815a64cd6360222099d7fc896682755882e68f0f
Josh Latour <jlatour88@gmail.com>
2019-09-16 21:34:09 -0500

    started work on retrieving and displaying ajax calls

--- client/src/components/booksSaved/SavedContainer.js
+++ client/src/components/booksSaved/SavedContainer.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios'
 
-const ResultsContainer = () => {
-   return (
-      <div>
-         <h3>Saved Books</h3>
-      </div>
-   )
+class SavedContainer extends Component {
+   state = {
+      books: []
+   };
+
+   componentDidMount() {
+      this.loadBooks();
+   }
+
+   loadBooks = () => {
+      axios.get("/api/books")
+         .then(res => {
+            this.setState({
+               books: res.data
+            })
+            console.log(this.state.books);
+         }
+         )
+         .catch(err => console.log(err));
+   };
+
+   render() {
+      return (
+         <div>
+            <h3>Saved Results</h3>
+         </div>
+      );
+   }
 }
 
-export default ResultsContainer;
+export default SavedContainer;
