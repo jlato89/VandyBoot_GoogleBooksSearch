8b9860188bc298d383bd0fd18536982dd7c28674
Josh Latour <jlatour88@gmail.com>
2019-09-16 19:21:48 -0500

    Moved Jumbotron to main template

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
-import SearchJumboTron from '../components/bookSearch/SearchJumboTron';
 import BookSearchForm from '../components/bookSearch/BookSearchForm';
 import ResultsContainer from '../components/bookSearch/ResultsContainer';
 import BookResults from '../components/bookSearch/BookResults';
@@ -9,7 +8,6 @@ class App extends Component {
    render() {
       return (
          <div className='app'>
-            <SearchJumboTron />
             <BookSearchForm />
             <ResultsContainer />
             <BookResults />
