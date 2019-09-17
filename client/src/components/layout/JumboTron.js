67c42daa92370e171bd37da006f92c5824a5efc9
Josh Latour <jlatour88@gmail.com>
2019-09-16 19:22:09 -0500

    Converted to main template

---
+++ client/src/components/layout/JumboTron.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function JumboTron() {
+   return (
+      <div className='jumbotron jumbotron-fluid'>
+         <div className='container'>
+            <center>
+               <h1 className='display-4'>(React) Google Books Search</h1>
+               <p className='lead'>Search for and Save Books of Interest</p>
+            </center>
+         </div>
+      </div>
+   );
+}
+
+export default JumboTron
