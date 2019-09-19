27c4832df81a9d99b7063eb943bfad7d521d500f
Josh Latour <jlatour88@gmail.com>
2019-09-19 08:50:31 -0500

    added state template

--- client/src/components/BookSearchForm.js
+++ client/src/components/BookSearchForm.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react'
 
 class BookSearch extends Component {
+   state = {
+      bookSearch: '',
+      bookResults: [],
+      bookTitle: '',
+      bookAuthors: [],
+      bookDescription: '',
+      bookImage: '',
+      bookLink: ''
+   };
    render() {
       return (
          <form>
             <div className='form-group'>
                <h4>Book Search</h4>
                <input
+                  name='bookSearch'
                   type='text'
                   className='form-control'
                   placeholder='Start your search here'
                ></input>
-               <button 
-               type='submit' 
-               className='btn btn-primary'
-               style={{float: 'right'}}
-               >Search
+               <button
+                  type='submit'
+                  className='btn btn-primary'
+                  style={{ float: 'right' }}
+               >
+                  Search
                </button>
             </div>
          </form>
